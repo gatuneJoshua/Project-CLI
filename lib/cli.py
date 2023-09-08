5b18feb70528c3b7b9e61a95523c76f9b3dc7dac
Joshua <joshua.gatune@student.moringaschool.com>
2023-09-08 03:37:45 +0300

    initial commit

--- lib/cli.py
+++ lib/cli.py
@@ -1,15 +1,51 @@
 import click
-from lib.commands import author, book, borrower, checkout
+
+# Dummy data for demonstration purposes
+books_database = []
+authors_database = []
 
 @click.group()
 def library():
-    """Library M.S."""
+    """Library Management CLI"""
     pass
 
-library.add_command(author.author)
-library.add_command(book.book)
-library.add_command(borrower.borrower)
-library.add_command(checkout.checkout)
+@library.command()
+@click.argument("title")
+@click.argument("author")
+def book(title, author):
+    """Manage books"""
+    add_book(title, author)
+    print("Book added successfully.")
 
-if __name__ == '__main__':
+@library.command()
+@click.argument("name")
+def author(name):
+    """Manage authors"""
+    add_author(name)
+    print("Author added successfully.")
+
+@library.command()
+@click.argument("entity", type=click.Choice(["books", "authors"]))
+def list(entity):
+    """List books or authors"""
+    if entity == "books":
+        list_books()
+    elif entity == "authors":
+        list_authors()
+
+def add_book(title, author):
+    books_database.append({"title": title, "author": author})
+
+def add_author(name):
+    authors_database.append({"name": name})
+
+def list_books():
+    for book in books_database:
+        print(f"Title: {book['title']}, Author: {book['author']}")
+
+def list_authors():
+    for author in authors_database:
+        print(f"Author: {author['name']}")
+
+if __name__ == "_main_":
     library()
